393696720d43a980670d7b5dacf1aad065709131
joe boone <joejoboone@hotmail.com>
2024-04-14 17:04:31 +0200

    worked on wpf

--- WPFFractionCalculator/App.xaml.cs
+++ WPFFractionCalculator/App.xaml.cs
@@ -1,6 +1,13 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FractionLibrary;
 
 namespace WPFFractionCalculator
 {
@@ -9,6 +16,7 @@ namespace WPFFractionCalculator
     /// </summary>
     public partial class App : Application
     {
+
     }
 
 }
--- WPFFractionCalculator/MainWindow.xaml.cs
+++ WPFFractionCalculator/MainWindow.xaml.cs
@@ -8,6 +8,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using FractionLibrary;
+using System;
+
 
 namespace WPFFractionCalculator
 {
@@ -16,9 +20,124 @@ namespace WPFFractionCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+        new Fraction firstFraction = new Fraction();
+        new Fraction secondFraction = new Fraction();
+        new Fraction resultFraction = new Fraction();
+
+
         public MainWindow()
         {
             InitializeComponent();
+            
         }
+
+        private void Button_Result(object sender, RoutedEventArgs e)
+        {
+            if (Operator.Text == "+" )
+            {
+                firstFraction = new Fraction(int.Parse(txtNumeratorone.Text), int.Parse(txtDenominatorone.Text));
+                secondFraction = new Fraction(int.Parse(txtNumeratortwo.Text), int.Parse(txtDenominatortwo.Text));
+                resultFraction = firstFraction.Add(secondFraction).Simplify();
+                txtDenominatorResult.Text = resultFraction.Denominator.ToString();
+                txtNumeratorResult.Text = resultFraction.Numerator.ToString();
+            }
+            else if(Operator.Text == "-")
+            {
+                firstFraction = new Fraction(int.Parse(txtNumeratorone.Text), int.Parse(txtDenominatorone.Text));
+                secondFraction = new Fraction(int.Parse(txtNumeratortwo.Text), int.Parse(txtDenominatortwo.Text));
+                resultFraction = firstFraction.Substract(secondFraction).Simplify();
+                txtDenominatorResult.Text = resultFraction.Denominator.ToString();
+                txtNumeratorResult.Text = resultFraction.Numerator.ToString();
+            }
+            else if(Operator.Text == "/")
+            {
+                firstFraction = new Fraction(int.Parse(txtNumeratorone.Text), int.Parse(txtDenominatorone.Text));
+                secondFraction = new Fraction(int.Parse(txtNumeratortwo.Text), int.Parse(txtDenominatortwo.Text));
+                resultFraction = firstFraction.Divide(secondFraction).Simplify();
+                txtDenominatorResult.Text = resultFraction.Denominator.ToString();
+                txtNumeratorResult.Text = resultFraction.Numerator.ToString();
+            }
+            else if(Operator.Text == "x")
+            {
+                firstFraction = new Fraction(int.Parse(txtNumeratorone.Text), int.Parse(txtDenominatorone.Text));
+                secondFraction = new Fraction(int.Parse(txtNumeratortwo.Text), int.Parse(txtDenominatortwo.Text));
+                resultFraction = firstFraction.Multiply(secondFraction).Simplify();
+                txtDenominatorResult.Text = resultFraction.Denominator.ToString();
+                txtNumeratorResult.Text = resultFraction.Numerator.ToString();
+            }
+            else if(Operator.Text == "-f")
+            {
+                firstFraction = new Fraction(int.Parse(txtNumeratorone.Text), int.Parse(txtDenominatorone.Text));
+                secondFraction = new Fraction(int.Parse(txtNumeratortwo.Text), int.Parse(txtDenominatortwo.Text));
+                resultFraction = firstFraction.Reciprocal().Simplify();
+                txtDenominatorResult.Text = resultFraction.Denominator.ToString();
+                txtNumeratorResult.Text = resultFraction.Numerator.ToString();
+                txtNumeratortwo.Visibility = Visibility.Hidden;
+                txtDenominatortwo.Visibility = Visibility.Hidden;
+                streeptwee.Visibility = Visibility.Hidden;
+                txtMessageotd.Text = "The second fraction is not used in this operation.";
+            }
+            else if(Operator.Text == "1/f")
+            {
+                firstFraction = new Fraction(int.Parse(txtNumeratorone.Text), int.Parse(txtDenominatorone.Text));
+                secondFraction = new Fraction(int.Parse(txtNumeratortwo.Text), int.Parse(txtDenominatortwo.Text));
+                resultFraction = firstFraction.Invert().Simplify();
+                txtDenominatorResult.Text = resultFraction.Denominator.ToString();
+                txtNumeratorResult.Text = resultFraction.Numerator.ToString();
+                txtNumeratortwo.Visibility = Visibility.Hidden;
+                txtDenominatortwo.Visibility = Visibility.Hidden;
+                streeptwee.Visibility = Visibility.Hidden;
+                txtMessageotd.Text = "The second fraction is not used in this operation.";
+                
+            }
+            else if (txtDenominatorone.Text == "" || txtNumeratorone.Text == "" || txtDenominatortwo.Text == "" || txtNumeratortwo.Text == ""  )
+            {
+                txtMessageotd.Text = "Please fill in all fields.";
+            }
+        }
+        
+
+        private void AddOperator_Click(object sender, RoutedEventArgs e)
+        {
+            Operator.Text = "+";
+        }
+
+        private void SubstractOperator_Click(object sender, RoutedEventArgs e)
+        {
+            Operator.Text = "-";
+        }
+
+        private void DivideOperator_Click(object sender, RoutedEventArgs e)
+        {
+            Operator.Text = "/";
+        }
+
+        private void MultiplyOperator_Click(object sender, RoutedEventArgs e)
+        {
+            Operator.Text = "x";
+        }
+
+        private void ReciprocalOperator_Click(object sender, RoutedEventArgs e)
+        {
+            Operator.Text = "-f";
+        }
+
+        private void InvertOperator_Click(object sender, RoutedEventArgs e)
+        {
+            Operator.Text = "1/f";
+        }
+        private void Button_Clear(object sender, RoutedEventArgs e)
+        {
+            txtNumeratorone.Text = "";
+            txtDenominatorone.Text = "";
+            txtNumeratortwo.Text = "";
+            txtDenominatortwo.Text = "";
+            txtNumeratorResult.Text = "";
+            txtDenominatorResult.Text = "";
+            txtMessageotd.Text = "";
+        }
+
+       
     }
 }
